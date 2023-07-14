75e6d669331c9d974776a2a94b283e535ccc19f9
Girish@0201 <bathigirish@gmail.com>
2023-07-14 21:25:05 +0530

    first commit

---
+++ src/Components/FooterCopy/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { AiOutlineHeart} from "react-icons/ai"
+import "./index.css"
+
+const FooterCopy = () => {
+  return (
+      <div className = "footer-copy">
+        <p>
+            Made with
+            <AiOutlineHeart className = "icon"/>
+            by team brothers
+        </p>
+    </div>
+  )
+}
+
+export default FooterCopy
+
